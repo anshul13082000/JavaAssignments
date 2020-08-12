93240bdaaa45b010e1657dcb282fd11e9ccc7256
Anshul Sharma <Anshul Sharma@DESKTOP-B4SOP58>
2020-08-12 11:21:44 +0530

    This is the First One.

---
+++ SBIAmazonProblem/src/Problem/SBIAmazonProblem.java
@@ -0,0 +1,28 @@
+package Problem;
+
+public class SBIAmazonProblem {
+
+	public static void main(String[] args) {
+		//Anshul
+		char ch1 = '\u0905',ch2 = '\u0902';
+		char ch3 = '\u0936',ch4 = '\u0941';
+		char ch5 = '\u0932';
+		
+		System.out.print(ch1);
+		System.out.print(ch2);
+		System.out.print(ch3);
+		System.out.print(ch4);
+		System.out.print(ch5+ " ");
+		
+		//Sharma
+		char ch6 = '\u0936', ch7 = '\u092E';
+		char ch8 = '\u093E', ch9 = '\u0930';
+		
+		System.out.print(ch6);
+		System.out.print(ch9);
+		System.out.print(ch7);
+		System.out.print(ch8);
+		
+	}
+
+}
---
+++ SBIAmazonProblem/src/Problem/TripleShift.java
@@ -0,0 +1,16 @@
+package Problem;
+
+public class TripleShift {
+
+	public static void main(String[] args) {
+		
+//		In Java, the operator ‘>>>’ is unsigned right shift operator. 
+//		It always fills 0 irrespective of the sign of the number.
+		int a = -1;
+		int b = a>>>29;
+		System.out.println(b);
+		
+
+	}
+
+}
